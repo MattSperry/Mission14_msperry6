ee421d1cb84734099984cbbf566f89fa9884a387
Matthew Sperry <106117544+MattSperry@users.noreply.github.com>
2023-04-04 16:24:21 -0600

    Add files via upload

---
+++ backend/MovieDatabase/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieDatabase.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDBContext context;
+        public MovieController(MovieDBContext temp) {
+            context = temp;
+        }
+        public IEnumerable<Movies> Get()
+        {
+            var x = context.Movies.ToArray();
+
+            return context.Movies.ToArray();
+        }
+    }
+}
---
+++ backend/MovieDatabase/Data/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Data
+{
+    public class Movies
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+
+    }
+}
